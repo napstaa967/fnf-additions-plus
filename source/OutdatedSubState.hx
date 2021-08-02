243afffc3c0186dc775c257d7581b7a2e6d49246
InsNameHere <57607350+napstaa967@users.noreply.github.com>
2021-08-01 20:00:31 -0500

    FIRST PUB RELEASE LMAO

---
+++ source/OutdatedSubState.hx
@@ -0,0 +1,27 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxSubState;
+import flixel.text.FlxText;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
+import lime.app.Application;
+
+class OutdatedSubState extends MusicBeatState
+{
+	public static var leftState:Bool = false;
+
+	public static var needVer:String = "IDFK LOL";
+	public static var currChanges:String = "dk";
+	
+	private var bgColors:Array<String> = [
+		'#314d7f',
+		'#4e7093',
+		'#70526e',
+		'#594465'
+	];
+	private var colorRotation:Int = 1;
+}
